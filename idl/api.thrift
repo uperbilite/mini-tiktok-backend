6008bf7474e9fad31e273bc9d0e429b12fabd36b
Bilite Deng <uperbilite@gmail.com>
2023-01-27 15:28:41 +0800

    add create user idl for user register.

--- idl/api.thrift
+++ idl/api.thrift
@@ -12,6 +12,19 @@ struct CheckUserResponse {
     4: string token
 }
 
+struct CreateUserRequest {
+    1: string username (api.query="username", api.vd="len($) > 0 && len($) <= 32")
+    2: string password (api.query="password", api.vd="len($) > 0 && len($) <= 32")
+}
+
+struct CreateUserResponse {
+    1: i64 status_code
+    2: string status_msg
+    3: i64 user_id
+    4: string token
+}
+
 service ApiService {
     CheckUserResponse CheckUser(1: CheckUserRequest req) (api.post="/douyin/user/login/")
+    CreateUserResponse CreateUser(1: CreateUserRequest req) (api.post="/douyin/user/register/")
 }
