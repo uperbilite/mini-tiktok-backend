f7fb76f8ade0c9e2ff75553b05d789716dc9d1b1
Bilite Deng <uperbilite@gmail.com>
2023-01-27 16:54:57 +0800

    add create user in user idl for rpc.

--- idl/user.thrift
+++ idl/user.thrift
@@ -2,7 +2,7 @@ namespace go user
 
 struct BaseResp {
     1: i64 status_code
-    2: string status_message
+    2: string status_msg
     3: i64 service_time
 }
 
@@ -16,6 +16,16 @@ struct CheckUserResponse {
     2: BaseResp base_resp
 }
 
+struct CreateUserRequest {
+    1: string username (vt.min_size = "1")
+    2: string password (vt.min_size = "1")
+}
+
+struct CreateUserResponse {
+    1: BaseResp base_resp
+}
+
 service UserService {
     CheckUserResponse CheckUser(1: CheckUserRequest req)
+    CreateUserResponse CreateUser(1: CreateUserRequest req)
 }
