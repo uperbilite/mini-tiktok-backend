cbc7af900483de429e2871a19f2250ba63b9bdd9
uperbilite <uperbilite@gmail.com>
2023-02-06 15:59:25 +0800

    use unix nano second as created_at, updated_at and deleted_at time.

--- pkg/configs/sql/init.sql
+++ pkg/configs/sql/init.sql
@@ -3,9 +3,9 @@ CREATE TABLE `users`
     `id`         bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'PK',
     `username`   varchar(128) NOT NULL DEFAULT '' COMMENT 'Username',
     `password`   varchar(128) NOT NULL DEFAULT '' COMMENT 'Password',
-    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'User account create time',
-    `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'User account update time',
-    `deleted_at` timestamp NULL DEFAULT NULL COMMENT 'User account delete time',
+    `created_at` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT 'User account create time',
+    `updated_at` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT 'User account update time',
+    `deleted_at` timestamp(3) NULL DEFAULT NULL COMMENT 'User account delete time',
     PRIMARY KEY (`id`),
     KEY          `idx_username` (`username`) COMMENT 'Username index'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='User account table';
@@ -19,9 +19,9 @@ CREATE TABLE `videos`
     `favourite_count` int(64) unsigned NOT NULL DEFAULT 0 COMMENT 'Favourite count',
     `comment_count`   int(64) unsigned NOT NULL DEFAULT 0 COMMENT 'Comment count',
     `title`           varchar(255) NOT NULL DEFAULT '' COMMENT 'Video title',
-    `created_at`      timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Video create time',
-    `updated_at`      timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Video update time',
-    `deleted_at`      timestamp NULL DEFAULT NULL COMMENT 'Video delete time',
+    `created_at`      timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT 'Video create time',
+    `updated_at`      timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT 'Video update time',
+    `deleted_at`      timestamp(3) NULL DEFAULT NULL COMMENT 'Video delete time',
     PRIMARY KEY (`id`),
     KEY               `idx_author_id` (`author_id`) COMMENT 'Author id index'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Video table';
@@ -31,9 +31,9 @@ CREATE TABLE `relations`
     `id`            bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'PK',
     `from_id`       bigint unsigned NOT NULL COMMENT 'From user id',
     `to_id`         bigint unsigned NOT NULL COMMENT 'To user id',
-    `created_at`    timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Relation create time',
-    `updated_at`    timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Relation update time',
-    `deleted_at`    timestamp NULL DEFAULT NULL COMMENT 'Relation delete time',
+    `created_at`    timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT 'Relation create time',
+    `updated_at`    timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT 'Relation update time',
+    `deleted_at`    timestamp(3) NULL DEFAULT NULL COMMENT 'Relation delete time',
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Relation table';
 
@@ -42,9 +42,9 @@ CREATE TABLE `favourites`
     `id`            bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'PK',
     `user_id`       bigint unsigned NOT NULL COMMENT 'User id',
     `video_id`      bigint unsigned NOT NULL COMMENT 'Video id',
-    `created_at`    timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Favourite create time',
-    `updated_at`    timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Favourite update time',
-    `deleted_at`    timestamp NULL DEFAULT NULL COMMENT 'Favourite delete time',
+    `created_at`    timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT 'Favourite create time',
+    `updated_at`    timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT 'Favourite update time',
+    `deleted_at`    timestamp(3) NULL DEFAULT NULL COMMENT 'Favourite delete time',
     PRIMARY KEY (`id`),
     KEY             `idx_user_id` (`user_id`) COMMENT 'User id index'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Favourite table';
