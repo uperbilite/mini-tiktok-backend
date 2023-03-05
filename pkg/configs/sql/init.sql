47bc6e5687a3c7837620d4b6a69a48fb856cd6ac
uperbilite <uperbilite@gmail.com>
2023-03-05 17:26:10 +0800

    ADD: count field in users and videos & remove index in follows and messages.

--- pkg/configs/sql/init.sql
+++ pkg/configs/sql/init.sql
@@ -1,27 +1,31 @@
 CREATE TABLE `users`
 (
-    `id`         bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'PK',
-    `username`   varchar(128) NOT NULL DEFAULT '' COMMENT 'Username',
-    `password`   varchar(128) NOT NULL DEFAULT '' COMMENT 'Password',
-    `created_at` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT 'User account create time',
-    `updated_at` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP (3) COMMENT 'User account update time',
-    `deleted_at` timestamp(3) NULL DEFAULT NULL COMMENT 'User account delete time',
+    `id`             bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'PK',
+    `username`       varchar(128) NOT NULL DEFAULT '' COMMENT 'Username',
+    `password`       varchar(128) NOT NULL DEFAULT '' COMMENT 'Password',
+    `follow_count`   int unsigned NOT NULL DEFAULT 0 COMMENT 'User follow count',
+    `follower_count` int unsigned NOT NULL DEFAULT 0 COMMENT 'User follower count',
+    `created_at`     timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT 'User account create time',
+    `updated_at`     timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP (3) COMMENT 'User account update time',
+    `deleted_at`     timestamp(3) NULL DEFAULT NULL COMMENT 'User account delete time',
     PRIMARY KEY (`id`),
-    KEY          `idx_username` (`username`) COMMENT 'Username index'
+    KEY              `idx_username` (`username`) COMMENT 'Username index'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='User account table';
 
 CREATE TABLE `videos`
 (
-    `id`         bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'PK',
-    `author_id`  bigint unsigned NOT NULL COMMENT 'Author id',
-    `play_url`   varchar(255) NOT NULL DEFAULT '' COMMENT 'Play video url',
-    `cover_url`  varchar(255) NOT NULL DEFAULT '' COMMENT 'Video cover url',
-    `title`      varchar(255) NOT NULL DEFAULT '' COMMENT 'Video title',
-    `created_at` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT 'Video create time',
-    `updated_at` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP (3) COMMENT 'Video update time',
-    `deleted_at` timestamp(3) NULL DEFAULT NULL COMMENT 'Video delete time',
+    `id`             bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'PK',
+    `author_id`      bigint unsigned NOT NULL COMMENT 'Author id',
+    `play_url`       varchar(255) NOT NULL DEFAULT '' COMMENT 'Play video url',
+    `cover_url`      varchar(255) NOT NULL DEFAULT '' COMMENT 'Video cover url',
+    `title`          varchar(255) NOT NULL DEFAULT '' COMMENT 'Video title',
+    `favorite_count` int unsigned NOT NULL DEFAULT 0 COMMENT 'Video favorite count',
+    `comment_count`  int unsigned NOT NULL DEFAULT 0 COMMENT 'Video Favorite count',
+    `created_at`     timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT 'Video create time',
+    `updated_at`     timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP (3) COMMENT 'Video update time',
+    `deleted_at`     timestamp(3) NULL DEFAULT NULL COMMENT 'Video delete time',
     PRIMARY KEY (`id`),
-    KEY          `idx_author_id` (`author_id`) COMMENT 'Author id index'
+    KEY              `idx_author_id` (`author_id`) COMMENT 'Author id index'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Video table';
 
 CREATE TABLE `follows`
@@ -32,9 +36,7 @@ CREATE TABLE `follows`
     `created_at` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT 'Follow create time',
     `updated_at` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP (3) COMMENT 'Follow update time',
     `deleted_at` timestamp(3) NULL DEFAULT NULL COMMENT 'Follow delete time',
-    PRIMARY KEY (`id`),
-    KEY          `idx_from_id` (`from_id`) COMMENT 'from_id index',
-    KEY          `idx_to_id` (`to_id`) COMMENT 'to_id index'
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Follow table';
 
 CREATE TABLE `messages`
@@ -46,9 +48,7 @@ CREATE TABLE `messages`
     `created_at` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT 'Message create time',
     `updated_at` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP (3) COMMENT 'Message update time',
     `deleted_at` timestamp(3) NULL DEFAULT NULL COMMENT 'Message delete time',
-    PRIMARY KEY (`id`),
-    KEY          `idx_user_id` (`user_id`) COMMENT 'user_id index',
-    KEY          `idx_to_user_id` (`to_user_id`) COMMENT 'to_user_id index'
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Message table';
 
 CREATE TABLE `favorites`
